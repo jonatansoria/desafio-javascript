21033e03266e3b37b5fd576d5904977cde5c765c
jonatan soria <jonatansoriatw@gmail.com>
2022-05-31 00:05:26 -0300

    primer desafio

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,13 @@
 
-let nombre = prompt ("ingrese su nombre");
-let apellido = prompt ("ingrese su apellido");
-let edad = prompt ("ingerese su edad"); //debe ser mayor de 18 para ingresar.
+let nombre = prompt ("Hola, Por favor ingrese su nombre para continuar");
+    alert ("Nombre imgresado "+nombre);
 
-if ((nombre != "") && (apellido != "") && ( edad >= 18)){
+let apellido = prompt ("Ahora ingrese su apellido");
+   alert  ("Hola "+nombre+" "+apellido);
 
-    alert ("Nombre: "+nombre+"\nApellido: "+apellido+"\nEdad: "+edad)
-}
+let edad = prompt ("ingrese tambien su edad"); //debe ser mayor de 18 para ingresar.
 
-else {
-    alert("debes ser mayor de edad para ingresar");
-}
+while (edad <=18) {
+    edad = prompt ("ingresa nuevamente tu edad");
+   
+} alert ("Bienvenido "+nombre+" "+apellido+"\nEdad "+edad+"años")
